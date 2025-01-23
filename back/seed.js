5ef1fb78f6c7fce819d50f5b4513651ff934a381
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-01-23 10:21:20 +0100

    Updating back from an older project

--- back/seed.js
+++ back/seed.js
@@ -5,9 +5,7 @@ const bcrypt = require('bcrypt');
 const sequelize = require('./src/config/database');
 
 // Importing models
-const Task = require('./src/models/task');
 const Category = require('./src/models/category');
-const User = require('./src/models/user');
 
 // Importing helpers
 const logConsole = require('./src/helpers/logConsole')
@@ -21,26 +19,15 @@ const seedData = async () => {
 
         // Drop database tables (disable and re-enable foreign key checks)
         await sequelize.query('PRAGMA foreign_keys = OFF');
-        await Task.drop();
-        await User.drop();
         await Category.drop();
         await sequelize.query('PRAGMA foreign_keys = ON');
 
         // Synchronize the database (drop all existing tables and recreate them)
         await sequelize.sync({ force: true });
 
-        // Sample data for User table, with crypted passwords
-        const sampleUsers = require('./src/seed_data/users.json');
-        for (const user of sampleUsers) user.password = await bcrypt.hash(user.password, 10);
-        await User.bulkCreate(sampleUsers);
-
         // Sample data for Category table
         const sampleCategories = require('./src/seed_data/categories.json');
         await Category.bulkCreate(sampleCategories);
-
-        // Sample data for Task table
-        const sampleTasks = require('./src/seed_data/tasks.json');
-        await Task.bulkCreate(sampleTasks);
 
         logConsole('Sample data inserted');
     } catch (error) {
@@ -50,24 +37,9 @@ const seedData = async () => {
     }
 };
 
-const checkData = async () => {
-    try {
-        // Verify that the data has been inserted with a SELECT query
-        const tasks = await Task.findAll();
-        if (tasks.length === 0) {
-            console.error('No tasks found in the database');
-        } else {
-            logConsole('Sample data verified');
-        }
-    } catch (error) {
-        logConsole(error, 'error');
-    }
-};
-
 (async () => {
     try {
         await seedData();
-        await checkData();
         logConsole('✅ Data seeding completed ✅');
         process.exit(0); // Terminate the script
     } catch (error) {
