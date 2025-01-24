a801ad83bdfa67106778454b23cb75faf48769e6
Yann <yann.sady.pro@gmail.com>
2025-01-24 09:05:19 +0100

    ✨ add zones

--- front/Ycata/src/app/app.component.ts
+++ front/Ycata/src/app/app.component.ts
@@ -3,15 +3,25 @@ import { Component, OnInit } from '@angular/core';
 import { filter } from 'rxjs';
 import { MapComponent } from './map/map.component';
 import { NavbarComponent } from "./navbar/navbar.component";
+import { Zone } from './zone/zone.model';
+import { ZoneComponent } from './zone/zone.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, NavbarComponent, MapComponent],
+  imports: [RouterModule, NavbarComponent, MapComponent, ZoneComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   title = 'Ycata';
+  zones: Zone[] = [
+    { id: 1, name: 'Zone 1', title: '', content: '' },
+    { id: 2, name: 'Zone 2', title: '', content: '' },
+    { id: 3, name: 'Zone 3', title: '', content: '' },
+    { id: 4, name: 'Zone 4', title: '', content: '' },
+    { id: 5, name: 'Zone 5', title: '', content: '' },
+  ];
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
