5998f629cefb8ec39c4009c0f75407645f79b507
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-01-24 09:05:51 +0100

    WIP

--- front/Ycata/src/app/app.component.ts
+++ front/Ycata/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { RouterOutlet } from '@angular/router';
 import { Component } from '@angular/core';
 import { MapComponent } from './map/map.component';
+import { ChatComponent } from './chat/chat.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MapComponent],
+  imports: [RouterOutlet, MapComponent, ChatComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
